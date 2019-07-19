967e1a74cc9f488dd37809e2c3e9979f8e7d0a8b
Takafumi Ogaito <go_aquarion.0454@icloud.com>
2019-07-19 14:45:08 +0900

    Update YPImagePicker.swift

--- Source/YPImagePicker.swift
+++ Source/YPImagePicker.swift
@@ -141,9 +141,9 @@ override open func viewDidLoad() {
         }
         
         // If user has not customized the Nav Bar tintColor, then use black.
-        if UINavigationBar.appearance().tintColor == nil {
-            UINavigationBar.appearance().tintColor  = .black
-        }
+        //if UINavigationBar.appearance().tintColor == nil {
+        //    UINavigationBar.appearance().tintColor  = .black
+        //}
     }
     
     deinit {
